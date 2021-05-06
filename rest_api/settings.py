c704f4e19cadf85287fb442c6b7762eef8de95c2
damletanmay <tanmay.damle.25@gmail.com>
2021-05-06 22:29:28 +0530

    Hosting Changes

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-rest-api-222.herokuapp.com/','localhost']
 
 
 # Application definition
